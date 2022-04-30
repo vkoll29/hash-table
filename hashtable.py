2d7ce0d7ab5b77fd2f20b1f19968e039080fb254
vkoll29 <awino_g@outlook.com>
2022-04-30 12:24:10 +0300

    Implemented Key/Value Retrieval - Can get the value responding to a key - Can check if a key exists - all test cases implemented and passed

--- hashtable.py
+++ hashtable.py
@@ -15,3 +15,43 @@ class HashTable:
         """
         index = hash(key) % len(self)
         self.values[index] = value
+
+
+    def __getitem__(self, key):
+        """
+        - get the hash value of the key which is its index
+        - access the value at that index
+        - check that the value is not BLANK object i.e has not been set. this means it does not have a key
+        - use is operator instead of == to compare identities not values
+        return value
+        """
+        index = hash(key) % len(self)
+        value = self.values[index]
+        if value is BLANK:
+            raise KeyError(key)
+        return value
+
+
+    def __contains__(self, key):
+        """
+        - find the value at key
+        - if a key error is raised return false
+        - else return true
+        """
+        try:
+            self[key]
+        except KeyError:
+            return False
+        else:
+            return True
+
+
+    def get(self, key, default=None):
+        """
+        - get the value at key
+        - if KeyError i.e missing key, return default which is an optional parameter
+        """
+        try:
+            return self[key]
+        except KeyError:
+            return default
--- test_hashtable.py
+++ test_hashtable.py
@@ -1,4 +1,15 @@
+import pytest
+
 from hashtable import HashTable, BLANK
+
+@pytest.fixture
+def hash_table():
+    sample_data = HashTable(size=100)
+    sample_data['hola'] = 'hello'
+    sample_data[24.9] = 43
+    sample_data[False] = True
+    return sample_data
+
 
 def test_should_create_hashtable():
     assert HashTable(size=100) is not None
@@ -22,10 +33,47 @@ def test_should_insert_key_value_pairs():
 
     assert len(hash_table) == 100
 
+
 def test_should_not_create_none_value_when_created():
     assert None not in HashTable(size=100).values
+
 
 def test_should_insert_none_value():
     hash_table = HashTable(size=100)
     hash_table['key'] = None
     assert None in hash_table.values
+
+
+def test_should_find_value_by_key(hash_table):
+    assert hash_table['hola'] == 'hello'
+    assert hash_table[24.9] == 43
+    assert hash_table[False] == True
+
+
+def test_should_raise_error_on_missing_key():
+    hash_table = HashTable(size=100)
+    with pytest.raises(KeyError) as exception_info:
+        hash_table['missing_key']
+    assert exception_info.value.args[0] == 'missing_key'
+
+
+# implementing the equivalent of dict's in method
+def test_should_find_key(hash_table):
+    assert 'hola' in hash_table
+
+
+def test_should_not_find_key(hash_table):
+    assert 'missing_key' not in hash_table
+
+# implementing the equivalent of dict's get method
+def test_should_get_value(hash_table):
+    assert hash_table.get('hola') == 'hello'
+
+def test_should_get_none_when_missing_key(hash_table):
+    assert hash_table.get('missing_key') is None
+
+def test_should_get_default_value_when_missing_key(hash_table):
+    assert hash_table.get('missing_key', 'default') == 'default'
+
+def test_should_get_value_with_default(hash_table):
+    assert hash_table.get('hola', 'default') == 'hello'
